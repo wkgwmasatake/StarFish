874741510aac56baadc4dde2a23848561517fbdc
LAPTOP-B3IIV33C\youjiro <youjiro-s-17d@stu.kbc.ac.jp>
2019-03-22 13:20:13 +0900

    カリのステージ追加

---
+++ Assets/Sawada/Script/BackGround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour {
+
+    //スプライトの高さ、幅
+    private float _height;
+
+	// Use this for initialization
+	void Start () {
+
+        SpriteRenderer obje = GetComponent<SpriteRenderer>();
+        _height = obje.bounds.size.y;
+	}
+
+    //ゲッター・セッター
+    public float height { get { return _height; } }
+}
---
+++ Assets/Sawada/Script/CameraMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    //背景のオブジェクト格納
+    [SerializeField] private GameObject BGPre;
+
+    //カメラの追従速度
+    [SerializeField] private float CameraSpeed;
+
+    //背景オブジェクトの情報格納
+    private BackGround BG;
+
+    //カメラの余白分
+    private int MARGIN = 5;
+
+    // Use this for initialization
+    void Start () {
+
+        BG = BGPre.GetComponent<BackGround>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if( BG.height/2 - MARGIN > transform.position.y ) transform.Translate(0, CameraSpeed * Time.deltaTime, 0);
+	}
+}
