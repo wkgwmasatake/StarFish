3e9ba066d75606f939b282e26e61308d26035947
INSPIRON-7567\Kento <kento2525@gmail.com>
2019-04-01 20:41:05 +0900

    エフェクトが自動で消えるように

---
+++ Assets/Higa/Script/AutoDelete.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDelete : MonoBehaviour {
+
+    private float lifetime;
+
+	// Use this for initialization
+	void Start () {
+
+        gameObject.transform.parent = null;
+
+        ParticleSystem partcleSystem = GetComponent<ParticleSystem>();
+        Destroy(gameObject, (float)partcleSystem.main.duration);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
