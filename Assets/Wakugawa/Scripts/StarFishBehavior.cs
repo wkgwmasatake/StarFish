428b0b00c3cb53cd6c623f0a94dcc52a931566f0
DESKTOP-SBQ2E7T\masatake <mstkwkgw@gmail.com>
2019-04-02 15:14:57 +0900

    // 一定時間長押ししている間矢印を表示、爆発力をSeliarizeFieldで宣言し、インスペクター上で変更可能に

--- Assets/Wakugawa/Scripts/StarFishBehavior.cs
+++ Assets/Wakugawa/Scripts/StarFishBehavior.cs
@@ -17,10 +17,13 @@ public class StarFishBehavior : MonoBehaviour {
     const byte _MAX_LEG = 5;        // 腕の最大数
 
     byte armNum = 0;                // 現在の腕
+    float Presstime = 0;            // 画面を長押ししている時間
 
     SpriteRenderer[] LegSpriteRenderer; // 腕のスプライトレンダラー
 
-    [SerializeField] ParticleSystem[] ParticleList;       // パーティクルリスト(0.. 腕のパーティクル、1.. 爆発のパーティクル、2.. 花火のパーティクル)
+    [SerializeField] ParticleSystem[] ParticleList;     // パーティクルリスト(0.. 腕のパーティクル、1.. 爆発のパーティクル、2.. 花火のパーティクル)
+    [SerializeField] GameObject ArrowObject;            // 矢印のゲームオブジェクト
+    [SerializeField] float bombPower;                   // 爆発の大きさ
 
     public Sprite[] LegImages;      // 腕の画像(0.. 通常時、1.. 選択時、2、3.. 爆発後)
 
@@ -37,21 +40,39 @@ public class StarFishBehavior : MonoBehaviour {
 
         LegSpriteRenderer[0].sprite = LegImages[1];                     // 最初の腕を選択時の腕に画像を変更
 
+        ArrowObject.SetActive(false);                                   // 非アクティブに設定
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0) && GameDirector.Instance.GetArmNumber() > 0)        // 左クリックしたとき、かつタップの最大数以下の時
+        if(Input.GetMouseButton(0) && GameDirector.Instance.GetArmNumber() > 1 && GameDirector.Instance.GetArmNumber() <= _MAX_TAP + 1)     // 最初のタップと最後のタップ以外
         {
-            if(GameDirector.Instance.GetArmNumber() <= _MAX_TAP + 1 && GameDirector.Instance.GetArmNumber() > 1)        // 最初のタップと最後のタップ以外の時
+            Presstime += Time.deltaTime;        // 長押ししている時間を計測
+            if (Presstime > 0.5)                // 0.5秒間長押ししたら
+                ArrowObject.SetActive(true);    // アクティブに設定
+        }
+
+		if(Input.GetMouseButtonUp(0) && GameDirector.Instance.GetArmNumber() > 0)        // 左クリックしたとき、かつタップの最大数以下の時
+        {
+            Presstime = 0;                      // 長押しの時間を初期化
+
+            if (GameDirector.Instance.GetArmNumber() <= _MAX_TAP + 1 && GameDirector.Instance.GetArmNumber() > 1)        // 最初のタップと最後のタップ以外の時
             {
+                if (!ArrowObject.activeSelf)         // 非アクティブ状態なら
+                {
+                    ArrowObject.SetActive(true);     // アクティブ状態に設定
+                }
+
+                ArrowObject.GetComponent<ArrowDirector>().SetArrowPos(transform.GetChild(armNum + 1));        // 次の腕に応じた矢印の位置に設定
+                ArrowObject.SetActive(false);   // 非アクティブに設定
+
                 Instantiate(ParticleList[(int)PARTICLE.ARM],transform); // 海星の子に設定して泡のパーティクルを生成
                 Vector2 armPos = transform.GetChild(armNum).position;   // ヒエラルキービューの上から子オブジェクトのワールド座標を取得
                 float ForceX = transform.position.x - armPos.x;         // 本体と腕のx座標の差を求める(力を加えるx方向)
                 float ForceY = transform.position.y - armPos.y;         // 本体と腕のy座標の差を求める(力を加えるy方向)
-
+                
                 Rigidbody2D rb = GetComponent<Rigidbody2D>();           // Rigidbodyを取得
-                Vector2 force = new Vector2(ForceX * 30, ForceY * 30);  // 本体と腕の座標の差から力を設定
+                Vector2 force = new Vector2(ForceX * bombPower, ForceY * bombPower);  // 本体と腕の座標の差から力を設定
 
                 rb.angularVelocity = 0;                                 // 回転の力を0に戻す
 
@@ -76,7 +97,7 @@ public class StarFishBehavior : MonoBehaviour {
             else if(GameDirector.Instance.GetArmNumber() > _MAX_TAP)    // 最初のタップ
             {
                 Rigidbody2D rb = GetComponent<Rigidbody2D>();           // Rigidbodyを取得
-                Vector2 force = new Vector2(-5.0f, 5.0f);                // 力を設定
+                Vector2 force = new Vector2(-5.0f * bombPower / 20, 5.0f * bombPower / 20);                // 力を設定
                 rb.AddTorque(0.8f, ForceMode2D.Impulse);                // 一瞬のみ回転を加える
                 rb.AddForce(force, ForceMode2D.Impulse);                // 一瞬のみ力を加える
                 GameDirector.Instance.SetArmNumber(GameDirector.Instance.GetArmNumber() - 1);               // 腕の本数を1減算
