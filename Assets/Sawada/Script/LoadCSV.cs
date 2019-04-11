32ff4fb8a3f597b2274e5a782baa8f6578ee1de2
y_Sawada <youjiro-s-17d@stu.kbc.ac.jp>
2019-04-11 14:57:41 +0900

    CSV読み込み処理追加

--- Assets/Sawada/Script/LoadCSV.cs
+++ Assets/Sawada/Script/LoadCSV.cs
@@ -3,39 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class LoadCSV : MonoBehaviour {
+public class LoadCSV : MonoBehaviour
+{
+    private float[,] _LoadPos;
 
-	// Use this for initialization
-	void Start ()
-    {
-        if (!Load()) Debug.LogError("Not CSV");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    bool Load()
+    public int Load()
     {
         StreamReader sr = new StreamReader(@"Assets/Resources/Ghost_Record.csv");
 
-        if (sr == null) return false;
+        if (sr == null) return -1;
 
+        _LoadPos = new float[50, 2];  //２次元配列を用意（５０個分の座標とｘ、ｙ分の２個）
         string line;
-        float[,] LoadPos = new float[50, 2];  //２次元配列を用意（５０個分の座標とｘ、ｙ分の２個）
         int i = 0;
 
         while((line = sr.ReadLine()) != null)
         {
             string[] Spritline = line.Trim('(', ')').Split(',');  //読み込んだ１行に含まれている中かっこを取り除き、カンマで区切って読み込む
-            LoadPos[i, 0] = float.Parse(Spritline[0]);  //x座標を抽出
-            LoadPos[i, 1] = float.Parse(Spritline[1]);  //y座標を抽出
+            _LoadPos[i, 0] = float.Parse(Spritline[0]);  //x座標を抽出
+            _LoadPos[i, 1] = float.Parse(Spritline[1]);  //y座標を抽出
             i++;
         }
 
         sr.Close();
 
-        return true;
+        return i;
+    }
+
+    //ゲッター
+    public float[,] LoadPos
+    {
+        get { return _LoadPos; }
     }
 }
