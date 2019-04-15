c4ff5784668453bb42aac6123e268b433549de1d
y_Sawada <youjiro-s-17d@stu.kbc.ac.jp>
2019-04-15 15:40:37 +0900

    LoadCSV更新

--- Assets/Sawada/Script/LoadCSV.cs
+++ Assets/Sawada/Script/LoadCSV.cs
@@ -10,34 +10,52 @@ public class LoadCSV : MonoBehaviour
 
     public int Load()
     {
-        StreamReader sr = new StreamReader(@"Assets/Resources/Ghost_Record.csv");
+        string filePass;
 
+        //アンドロイドで実行している場合
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            //軌跡データのパスを保存
+            filePass = Application.persistentDataPath + "\\Resources\\Ghost_Record.csv";
+        }
+        else
+        {
+            //軌跡データのパスを保存
+            filePass = Application.dataPath + "\\Resources\\Ghost_Record.csv";
+        }
+
+        StreamReader sr = new StreamReader(filePass);
+
+        // --- 読み込みエラー ---//
         if (sr == null) return -1;
 
-        _LoadPos = new float[200, 2];  //２次元配列を用意
-        _LoadAngle = new float[200];
+        _LoadPos = new float[100, 2];
+        _LoadAngle = new float[100];
+
         string line;
         int i = 0;
+        int j = 0;
 
         while((line = sr.ReadLine()) != null)
         {
-            string[] Spritline = line.Trim('(', ')').Split(',');  //読み込んだ１行に含まれている中かっこを取り除き、カンマで区切って読み込む
-            if (i % 2 == 0)
+            if (i++ % 2 == 0)
             {
-                _LoadPos[i, 0] = float.Parse(Spritline[0]);  //x座標を抽出
-                _LoadPos[i, 1] = float.Parse(Spritline[1]);  //y座標を抽出
+                string[] Spritline = line.Trim('(', ')').Split(',');  //読み込んだ１行に含まれている中かっこを取り除き、カンマで区切って読み込む
+                //Debug.Log(Spritline[0]);
+                //Debug.Log(Spritline[1]);
+                _LoadPos[j, 0] = float.Parse(Spritline[0]);  //x座標を抽出
+                _LoadPos[j, 1] = float.Parse(Spritline[1]);  //y座標を抽出
             }
             else
             {
-                _LoadAngle[i] = float.Parse(line);
+                _LoadAngle[j] = float.Parse(line);
+                j++;
             }
-
-            i++;
         }
 
         sr.Close();
 
-        return i;
+        return j;
     }
 
     //ゲッター
