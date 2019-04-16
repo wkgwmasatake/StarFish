3837a5a7d30a03dae39ab741f0efbef3a1051efc
INSPIRON-7567\Kento <kento2525@gmail.com>
2019-04-16 15:56:26 +0900

    タイトルの花火が爆発するまでの流れを作成

---
+++ Assets/Higa/Title/Script/CloudMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour {
+
+    [SerializeField] float direction;       // 右なら 1 , 左なら -1 を入力
+    [SerializeField] float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var pos = this.transform.position;
+        pos.x += speed * direction;
+
+        this.transform.position = pos;
+
+	}
+}
---
+++ Assets/Higa/Title/Script/StarShine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarShine : MonoBehaviour {
+
+    private bool scaleflg;
+    private float startScale;
+
+	// Use this for initialization
+	void Start () {
+
+        scaleflg = false;
+        startScale = this.gameObject.transform.localScale.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(this.transform.localScale.x > 0f && scaleflg == false)
+        {
+            Vector3 scale = this.transform.localScale;
+            scale.x -= 0.01f;
+            scale.y -= 0.01f;
+            scale.z -= 0.01f;
+
+            this.transform.localScale = scale;
+
+        }
+        else if(scaleflg == false)
+        {
+            scaleflg = true;
+        }
+
+        if (this.transform.localScale.x < startScale && scaleflg == true)
+        {
+            Vector3 scale = this.transform.localScale;
+            scale.x += 0.01f;
+            scale.y += 0.01f;
+            scale.z += 0.01f;
+
+            this.transform.localScale = scale;
+        }
+        else if(scaleflg == true)
+        {
+            scaleflg = false;
+        }
+
+        Debug.Log(this.gameObject.transform.localScale);
+
+	}
+}
