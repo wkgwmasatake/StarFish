d01af8d30e76c744d8fa06d208cd0a3b5764899d
y_Sawada <youjiro-s-17d@stu.kbc.ac.jp>
2019-04-17 10:11:06 +0900

    Fontフォルダ作成 しらさぎフォント移動 UI調整

--- Assets/Sawada/Script/StarFishArmImage.cs
+++ Assets/Sawada/Script/StarFishArmImage.cs
@@ -10,12 +10,16 @@ public class StarFishArmImage : MonoBehaviour
 
     [SerializeField] private Image[] LegRender;
     [SerializeField] private Sprite legImage;
+    [SerializeField] private Text ArmTex;
 
     private const int _MAX_LEG = 5;
+
+    private Text ArmNumberText;
 
 	// Use this for initialization
 	void Start ()
     {
+        ArmNumberText = ArmTex.GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -23,13 +27,17 @@ public class StarFishArmImage : MonoBehaviour
     {
         ArmNumber = GameDirector.Instance.GetArmNumber();
 
-        Debug.Log("ArmNumber : " + ArmNumber);
-        Debug.Log("StartArmNumber : " + StartArmNum);
+        //Debug.Log("ArmNumber : " + ArmNumber);
+        //Debug.Log("StartArmNumber : " + StartArmNum);
 
         if (ArmNumber == StartArmNum && ArmNumber != 0 && StartArmNum != 0)
         {
             StartArmNum--;
-            if(StartArmNum >= 0 && LegRender[StartArmNum] != null) LegRender[StartArmNum].sprite = legImage;
+            if (StartArmNum >= 0 && LegRender[StartArmNum] != null)
+            {
+                LegRender[StartArmNum].sprite = legImage;
+                ArmNumberText.text = StartArmNum.ToString();
+            }
         }
 	}
 }
