b47df357bcd717505b54b0d2824f722dcf04b2b6
DESKTOP-SBQ2E7T\masatake <mstkwkgw@gmail.com>
2019-04-22 15:56:56 +0900

    // ステージデータのロード、セーブを行うスクリプトを作成しました。publicで宣言しているので仕様書を見て必要なところで呼び出してください。

--- Assets/Wakugawa/Scripts/LoadStageInfo.cs
+++ Assets/Wakugawa/Scripts/LoadStageInfo.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class LoadStageInfo : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // ステージのクリア状況を取得する際に呼び出す関数
+    public int LoadStageClear()
+    {
+        int StageStatus = GameDirector.Instance.GetStageClear_Flg;      // ステージのクリア状況を取得
+
+        // ステージのクリア状況が取得できなければ-1を返す
+        if(StageStatus <= 0)
+        {
+            return -1;
+        }
+
+        // 1が立っている最上位ビットを取得
+        StageStatus = ((StageStatus & 0xFF00) != 0) ? StageStatus & 0xFF00 : StageStatus;
+        StageStatus = ((StageStatus & 0xF0F0) != 0) ? StageStatus & 0xF0F0 : StageStatus;
+        StageStatus = ((StageStatus & 0xCCCC) != 0) ? StageStatus & 0xCCCC : StageStatus;
+        StageStatus = ((StageStatus & 0xAAAA) != 0) ? StageStatus & 0xAAAA : StageStatus;
+
+        // ステージをどこまでクリアしたかを取得し、その値がステージの最大数より大きければ-1を返す
+        if(Mathf.Log(StageStatus, 2) > GameDirector.Instance.GetSTAGE_MAX)
+        {
+            return -1;
+        }
+
+        return (int)Mathf.Log(StageStatus, 2);      // 求めた値の2の何乗かを出すことでどのステージまでクリアしているかを出すことができる。
+    }
 }
--- Assets/Wakugawa/Scripts/SaveStageInfo.cs
+++ Assets/Wakugawa/Scripts/SaveStageInfo.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class SaveStageInfo : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // ステージをクリアした際に呼び出す関数。引数にはステージ番号を持たせる。
+    public void SaveSatageClearInfo(int stageNo)
+    {
+        if(stageNo - 1 < 0)
+        {
+            return;
+        }
+
+        int StageStatus = GameDirector.Instance.GetStageClear_Flg;      // ステージのクリア状況を取得
+
+        if(StageStatus < 0)
+        {
+            return;
+        }
+        StageStatus = StageStatus | (int)Mathf.Pow(2, stageNo - 1);     // クリアしたステージの番号の2乗と現在のステージの和をステージのクリア状況として変数に格納
+
+        GameDirector.Instance.SetStageClear_Flg = StageStatus;          // 更新したステージのクリア状況をディレクターに渡す
+    }
+
+    // アプリケーション終了時にデータを保存するために呼び出す
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("STAGE", GameDirector.Instance.GetStageClear_Flg);       // ステージのクリア状況をPlayerPrefsに保存
+        PlayerPrefs.SetInt("AREA", GameDirector.Instance.GetAreaClear_Flg);         // エリアの制覇状況をPlayerPrefsに保存
+    }
 }
