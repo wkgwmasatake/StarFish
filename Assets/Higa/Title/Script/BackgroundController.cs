348af868c5e2a4911dd3cc86afd4550d49535a93
INSPIRON-7567\Kento <kento2525@gmail.com>
2019-04-23 14:41:57 +0900

    タイトルの波を変更

--- Assets/Higa/Title/Script/BackgroundController.cs
+++ Assets/Higa/Title/Script/BackgroundController.cs
@@ -6,8 +6,9 @@ public class BackgroundController : MonoBehaviour
 {
     void Update()
     {
-        float scroll = Mathf.Repeat(Time.time * 0.01f, 1);
-        Vector2 offset = new Vector2(0, scroll);
+        float scrollX = Mathf.Repeat(Time.time * 0.03f, 1);
+        float scrollY = Mathf.Repeat(Time.time * 0.06f, 1);
+        Vector2 offset = new Vector2(scrollX, scrollY);
         GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
     }
 }
