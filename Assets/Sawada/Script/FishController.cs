f3cbb6be7adef2fe00c601f1f0970abbfaa89b8a
y_Sawada <youjiro-s-17d@stu.kbc.ac.jp>
2019-04-26 13:49:38 +0900

    魚ギミック作成

---
+++ Assets/Sawada/Script/FishController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour
+{
+
+    /// <summary>
+    ///    パラメータ
+    /// </summary>
+    private float Speed;
+    private int vector;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        // タグ設定
+        this.tag = "Fish";
+
+        // レイヤー設定
+        this.gameObject.layer = LayerMask.NameToLayer("Fish");
+
+		// 生成された場所によってベクトル変更
+        if(transform.position.x < 0)
+        {
+            vector = 1;
+        }
+        else if(transform.position.x >= 0)
+        {
+            vector = -1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Speed * vector * Time.deltaTime, 0, 0);
+	}
+
+
+
+    /// <summary>
+    ///   
+    ///    速さ設定
+    ///   
+    /// </summary>
+    public float SetSpeed { set { Speed = value; } }
+}
---
+++ Assets/Sawada/Script/FishGenerator.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishGenerator : MonoBehaviour
+{
+
+    [SerializeField] private Sprite fish;
+    [SerializeField] private float span;
+    [SerializeField] private float speed;
+
+    private float time = 0;
+
+    private Vector2 pos;
+
+    private GameObject PARENT;
+
+    private void Start()
+    {
+        PARENT = new GameObject("Fish_Parent");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        time += Time.deltaTime;
+        if(time >= span)
+        {
+            // タイムリセット
+            time = 0;
+
+            // オブジェクト生成
+            GameObject fish = CreateObj();
+
+            // 位置リセット
+            fish.transform.position = Vector2.zero;
+
+            // 親設定  
+            fish.transform.parent = PARENT.transform;
+
+            // 生成される場所設定
+            fish.transform.position = transform.position;
+            if (fish.transform.position.x > 0) fish.transform.localScale = new Vector2(-1, 1);
+
+            // コライダー設定
+            CreateCollider(fish);
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// 　　　オブジェクト生成
+    /// 
+    /// </summary>
+    private GameObject CreateObj()
+    {
+        // オブジェクト生成
+        GameObject Fish = new GameObject("FishObj");
+
+        // スプライトレンダラー追加・スプライト追加
+        Fish.AddComponent<SpriteRenderer>().sprite = fish;
+        // レイヤー設定
+        Fish.GetComponent<SpriteRenderer>().sortingOrder = 2;
+
+        // スクリプト設定
+        Fish.AddComponent<FishController>();
+        // 移動速度設定
+        Fish.GetComponent<FishController>().SetSpeed = speed;
+
+        return Fish;
+    }
+
+
+
+    /// <summary>
+    /// 
+    ///      コライダー生成
+    /// 
+    /// </summary>
+    /// <param name="obj"></param>
+    void CreateCollider(GameObject obj)
+    {
+        // ポリゴンコライダー追加
+        obj.AddComponent<PolygonCollider2D>();
+    }
+}
