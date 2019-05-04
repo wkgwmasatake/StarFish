ab3edc55b6a50918ce9f7fb989c6c3fe258daf41
Chalice <tatsuki-k-17e@stu.kbc.ac.jp>
2019-05-04 11:02:57 +0900

    [更新]masterとのマージ用コミット

---
+++ Assets/Kaneshi/Secret/AreaButtonController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AreaButtonController : MonoBehaviour
+{
+    [SerializeField] public int areaNumber;
+    [SerializeField] private Sprite[] unlockSprite;     //解放時の画像
+    [SerializeField] private Sprite lockSprite;     //未解放時の画像
+
+    private byte releaseflg;        //解放しているかどうかのフラグ
+
+    private enum LOCKSTATE
+    {
+        LOCK,
+        UNLOCK
+    };
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        if (releaseflg == (byte)LOCKSTATE.LOCK)
+        {
+            GetComponent<Image>().sprite = lockSprite;
+        }
+        else
+        {
+            GetComponent<Image>().sprite = unlockSprite[areaNumber];
+        }
+
+    }
+
+    void SetReleaseFlg(byte value)
+    {
+        releaseflg = value;
+    }
+}
---
+++ Assets/Kaneshi/Secret/SecretAreaDirector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretAreaDirector : MonoBehaviour {
+
+    //
+
+
+    private void Awake()
+    {
+      //やりたいことリスト
+      //1.エリア解放状況を知りたい
+      //2.エリアを選ぶボタンをエリアの数生成したい
+      //3.エリア解放状況に応じて、エリアを選ぶボタンの画像を切り替えたい
+      //4.エリア解放状況に応じて、エリア簡易アイコンの画像を切り替えたい
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
