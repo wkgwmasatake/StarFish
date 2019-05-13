c67fc8dc50e1ec89791a135ec828724d32bf345d
y_Sawada <youjiro-s-17d@stu.kbc.ac.jp>
2019-05-13 10:37:40 +0900

    クラゲアニメーション追加

---
+++ Assets/Sawada/Script/JellyFish_Top.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyFish_Top : MonoBehaviour
+{
+    private bool _TopFlg = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Flg = true;
+        }
+    }
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if(collision.gameObject.tag == "Player")
+    //    {
+    //        Flg = false;
+    //    }
+    //}
+
+    public bool Flg
+    {
+        get { return _TopFlg; }
+        set { _TopFlg = value; }
+    }
+
+    public Vector2 GetDistance()
+    {
+        Vector2 distance;
+
+        distance.x = this.transform.position.x - this.transform.parent.gameObject.transform.position.x;
+        distance.y = this.transform.position.y - this.transform.parent.gameObject.transform.position.y;
+
+        return distance;
+    }
+
+}
