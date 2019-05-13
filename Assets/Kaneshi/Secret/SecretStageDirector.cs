09f5ef6fa801bd25896c749deb2b013526046751
Chalice <tatsuki-k-17e@stu.kbc.ac.jp>
2019-05-13 09:35:25 +0900

    [追加・修正]緊急用AreaSelectのスクリプト作成、修正

---
+++ Assets/Kaneshi/Secret/SecretStageDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SecretStageDirector : MonoBehaviour
+{
+
+    private int AreaNumber;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Kaneshi/Secret/StageButtonController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageButtonController : MonoBehaviour
+{
+    private int stageNumber;
+    [SerializeField] private Sprite[] unlockSprite;     //解放時の画像
+    [SerializeField] private Sprite lockSprite;     //未解放時の画像
+
+    private float originXposition;  //初期X座標
+    private GameObject Adirector;
+
+    private byte releaseflg;        //解放しているかどうかのフラグ
+
+    public enum LOCKSTATE
+    {
+        LOCK,
+        UNLOCK
+    };
+
+    private GameObject SDirector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SDirector = GameObject.Find("StageSelectDirector");
+        originXposition = transform.position.x;
+
+        if (releaseflg == (byte)LOCKSTATE.LOCK)
+        {
+            GetComponent<Image>().sprite = lockSprite;
+        }
+        else
+        {
+            GetComponent<Image>().sprite = unlockSprite[stageNumber];
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void SetReleaseFlg(byte value)
+    {
+        releaseflg = value;
+    }
+
+    public void SetStageNumber(int value)
+    {
+        stageNumber = value;
+    }
+
+}
