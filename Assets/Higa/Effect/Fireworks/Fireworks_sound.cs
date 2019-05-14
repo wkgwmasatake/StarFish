128422dcbaf12609f14f0860d17883d884771a96
INSPIRON-7567\Kento <kento2525@gmail.com>
2019-05-14 16:11:57 +0900

    花火の音が少し遅れるように

---
+++ Assets/Higa/Effect/Fireworks/Fireworks_sound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireworks_sound : MonoBehaviour {
+
+    [SerializeField] AudioSource[] AS;
+
+    private int array_n;
+
+	// Use this for initialization
+	void Start () {
+        array_n = AS.Length;
+        //Debug.Log(AS.Length);
+        Invoke("PlaySound", 0.6f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void PlaySound()
+    {
+        for(int i = 0; i < array_n; i++)
+        {
+            AS[i].Play();
+        }
+    }
+}
