7c705b122152e1b8264b765521ec131526298836
INSPIRON-7567\Kento <kento2525@gmail.com>
2019-05-14 13:08:14 +0900

    花火の改良

---
+++ Assets/Higa/Effect/Fireworks/particle_test.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particle_test : MonoBehaviour {
+
+    [SerializeField] AudioSource AS;
+    [SerializeField] AudioClip se_f_mini;
+
+    private ParticleSystem ps;
+    private bool emitflg;
+    private int psnum;
+
+
+	// Use this for initialization
+	void Start () {
+
+        ps = GetComponent<ParticleSystem>();
+        emitflg = false;
+        psnum = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(emitflg == false && ps.isPlaying)
+        {
+            //se_f_mini.Play();
+
+            emitflg = true;
+        }
+        //Debug.Log(ps.isEmitting);
+        Debug.Log(getSubEmitterParticleNum());
+        if (psnum < getSubEmitterParticleNum())
+        {
+            psnum = getSubEmitterParticleNum();
+        }
+        else if(psnum > getSubEmitterParticleNum())
+        {
+            AS.PlayOneShot(se_f_mini);
+            //AS.Play();
+            psnum = getSubEmitterParticleNum();
+        }
+        
+    }
+
+    private int getSubEmitterParticleNum()
+    {
+        int ptNum = 0;
+        ParticleSystem[] psArr = GetComponents<ParticleSystem>();
+        foreach (ParticleSystem ps in psArr)
+        {
+            ptNum += ps.particleCount;
+        }
+
+        //Debug.Log(ptNum);
+        return ptNum;
+    }
+}
