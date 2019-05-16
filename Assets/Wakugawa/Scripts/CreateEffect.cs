d9fe0b9848b672072d391311315f16a76c84c9bc
DESKTOP-SBQ2E7T\masatake <mstkwkgw@gmail.com>
2019-05-16 19:51:07 +0900

    // 開始時のアニメーションの修正と軌跡の保存をしないように変更しました

---
+++ Assets/Wakugawa/Scripts/CreateEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CreateEffect : MonoBehaviour {
+
+    [SerializeField] ParticleSystem SmokeParticle;        // 土煙のパーティクル
+
+    public void InstantiateParticle()
+    {
+        // 土煙のパーティクルを2つ生成
+        var SmokeRight = Instantiate(SmokeParticle);
+        var SmokeLeft = Instantiate(SmokeParticle);
+
+        // 左側のエフェクトを反転させる
+        SmokeLeft.transform.localScale = new Vector3(-1, 1, 1);
+    }
+}
