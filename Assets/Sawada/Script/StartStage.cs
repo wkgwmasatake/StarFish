8e021a9668859827851691c13b3ddfe2cd33730c
y_Sawada <youjiro-s-17d@stu.kbc.ac.jp>
2019-05-16 12:13:49 +0900

    TestStage１にのみフェードアウト処理追加

--- Assets/Sawada/Script/StartStage.cs
+++ Assets/Sawada/Script/StartStage.cs
@@ -6,19 +6,26 @@ using UnityEngine.UI;
 public class StartStage : MonoBehaviour
 {
     private string stageName;
+    private Animation anim;
     
 	// Use this for initialization
 	void Start ()
     {
         GetComponent<Text>().text = StageName_Configuration();
-        GetComponent<Animation>().Play();
+        anim = GetComponent<Animation>();
 	}
 
     private void Update()
     {
-        
+        // 自分自身のアニメーションが終わったら
+        if(!anim.IsPlaying(anim.name))
+        {
+            // プレイヤー側が動けるように
+            GameDirector.Instance.SetPauseFlg = false;
+        }
     }
 
+    // 今のステージシーンの名前を直接取得
     string StageName_Configuration()
     {
         stageName = GameDirector.Instance.GetSceneName;
