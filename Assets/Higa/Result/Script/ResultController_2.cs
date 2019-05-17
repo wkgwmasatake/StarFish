6f16d8b2f27d96d9c5f871cf1ddffc4e157282f2
INSPIRON-7567\Kento <kento2525@gmail.com>
2019-05-17 09:51:08 +0900

    タイトルとリザルトの演出を変更

---
+++ Assets/Higa/Result/Script/ResultController_2.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultController_2 : MonoBehaviour {
+
+    enum PHASE
+    {
+        FADE,
+        STAR,
+
+        END,
+    }
+
+    [SerializeField] Image whitefade;
+    [SerializeField] GameObject star;
+    [SerializeField] GameObject ShootingStar;
+
+    [SerializeField] AudioSource se_splash;
+
+    [SerializeField] float RisingTime;
+
+    private PHASE now_phase;
+
+    private GameObject _star;
+    private ParticleSystem _star_ps;
+    private float time;
+
+    private bool pawnflg1, pawnflg2, pawnflg3;
+
+	// Use this for initialization
+	void Start () {
+
+        whitefade.enabled = true;
+
+        ShootingStar.SetActive(false);
+
+        now_phase = PHASE.FADE;
+
+        pawnflg1 = pawnflg2 = pawnflg3 = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Debug.Log(GameDirector.Instance.GetArmNumber());
+        switch (now_phase)
+        {
+            case PHASE.FADE:
+                FadeProcess();
+                break;
+
+            case PHASE.STAR:
+                StarfishProcess();
+                break;
+
+        }
+
+
+	}
+
+    private void FadeProcess()
+    {
+
+        whitefade.rectTransform.anchoredPosition =
+                    new Vector2(whitefade.rectTransform.anchoredPosition.x, whitefade.rectTransform.anchoredPosition.y - 50);     // フェード画像のy座標を50下げる
+
+        //Debug.Log(whitefade.rectTransform.anchoredPosition);
+
+        if(whitefade.rectTransform.anchoredPosition.y < -2850f)
+        {
+            ChangePhase(PHASE.STAR);
+        }
+    }
+
+
+    private void StarfishProcess()
+    {
+
+        if(pawnflg1 == false)
+        {
+
+            _star = Instantiate(star);
+            //Rigidbody2D rb = _star.GetComponent<Rigidbody2D>();
+            _star_ps = _star.GetComponent<ParticleSystem>();
+
+            //Vector2 force = new Vector2(0f, 45f);
+            //rb.AddForce(force, ForceMode2D.Impulse);
+            //rb.AddTorque(5f, ForceMode2D.Impulse);
+
+            pawnflg1 = true;
+
+        }
+
+        time += Time.time;
+        if (time >= RisingTime)
+        {
+            if (GameObject.Find("GameDirector") != null)
+            {
+                GameDirector.Instance.ParticleFlg = true;
+                //Debug.Log("finish");
+            }
+            else
+            {
+                Debug.Log("Fireworks finished");
+            }
+
+            ChangePhase(PHASE.END);
+        }
+        //Debug.Log(GameDirector.Instance.ParticleFlg);
+    }
+
+
+    private void ChangePhase(PHASE p)
+    {
+
+        now_phase = p;
+
+        pawnflg1 = pawnflg2 = pawnflg3 = false;
+    }
+}
---
+++ Assets/Higa/Result/Script/Rising_Star.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rising_Star : MonoBehaviour {
+
+    [SerializeField] GameObject TargetPoint;
+    [SerializeField] float movetime;
+    [SerializeField] float angle;
+
+    private Vector3 targetPos;
+    private Vector3 startPos;
+
+    private float now_position;
+
+	// Use this for initialization
+	void Start () {
+
+        targetPos = TargetPoint.transform.position;
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if(transform.position != targetPos)
+        //{
+        //    float pos_y = transform.position.y;
+
+        //    transform.position = new Vector3(0, pos_y + 1 / (60 * movetime), 0);
+        //}
+
+        now_position += 1 / (60 * movetime);
+        transform.position = Vector3.Lerp(startPos, targetPos, now_position);
+
+
+
+    }
+}
