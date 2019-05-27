f6c3a2f230a569087841a0a530415f91155818ba
y_Sawada <youjiro-s-17d@stu.kbc.ac.jp>
2019-05-27 11:49:34 +0900

    フェード時にエフェクトが上にくるバグ修正

--- Assets/Sawada/Script/StartStage.cs
+++ Assets/Sawada/Script/StartStage.cs
@@ -31,55 +31,62 @@ public class StartStage : MonoBehaviour
     // 今のステージシーンの名前を直接取得
     string StageName_Configuration(int num)
     {
-        switch (num)
-        {
-            case 2:
-                name = StageName[0];
-                break;
-            case 3:
-                name = StageName[1];
-                break;
-            case 4:
-                name = StageName[2];
-                break;
-            case 5:
-                name = StageName[3];
-                break;
-            case 6:
-                name = StageName[4];
-                break;
-            case 7:
-                name = StageName[5];
-                break;
-            case 8:
-                name = StageName[6];
-                break;
-            case 9:
-                name = StageName[7];
-                break;
-            case 10:
-                name = StageName[8];
-                break;
-            case 11:
-                name = StageName[9];
-                break;
-            case 12:
-                name = StageName[10];
-                break;
-            case 13:
-                name = StageName[11];
-                break;
-            case 14:
-                name = StageName[12];
-                break;
-            case 15:
-                name = StageName[13];
-                break;
-            case 16:
-                name = StageName[14];
-                break;
-        }
+        return StageName[num - 2];
 
-        return name;
+        //switch (num)
+        //{
+        //    case 2:
+        //        name = StageName[0];
+        //        break;
+        //    case 3:
+        //        name = StageName[1];
+        //        break;
+        //    case 4:
+        //        name = StageName[2];
+        //        break;
+        //    case 5:
+        //        name = StageName[3];
+        //        break;
+        //    case 6:
+        //        name = StageName[4];
+        //        break;
+        //    case 7:
+        //        name = StageName[5];
+        //        break;
+        //    case 8:
+        //        name = StageName[6];
+        //        break;
+        //    case 9:
+        //        name = StageName[7];
+        //        break;
+        //    case 10:
+        //        name = StageName[8];
+        //        break;
+        //    case 11:
+        //        name = StageName[9];
+        //        break;
+        //    case 12:
+        //        name = StageName[10];
+        //        break;
+        //    case 13:
+        //        name = StageName[11];
+        //        break;
+        //    case 14:
+        //        name = StageName[12];
+        //        break;
+        //    case 15:
+        //        name = StageName[13];
+        //        break;
+        //    case 16:
+        //        name = StageName[14];
+        //        break;
+        //}
+
+        //return name;
+    }
+
+    private void OnDestroy()
+    {
+        GameDirector.Instance.SetPauseFlg = false;
     }
 }
