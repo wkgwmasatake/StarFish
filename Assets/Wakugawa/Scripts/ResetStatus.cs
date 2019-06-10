1687b16e9302b6252dad5dd84cec7337ded475d2
DESKTOP-SBQ2E7T\masatake <mstkwkgw@gmail.com>
2019-06-10 22:33:20 +0900

    // リザルトの修正とリザルトにステータスのリセットボタンを追加しました。

---
+++ Assets/Wakugawa/Scripts/ResetStatus.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ResetStatus : MonoBehaviour {
+
+    [SerializeField] Image FadeImage;           // フェード用画像
+    
+    // 今までのデータを削除
+    public void DeleteStatus()
+    {
+        // 今までのクリア状況をリセット
+        if (PlayerPrefs.HasKey("STAGE"))
+            PlayerPrefs.DeleteKey("STAGE");
+        if (PlayerPrefs.HasKey("AREA"))
+            PlayerPrefs.DeleteKey("AREA");
+
+        GameDirector.Instance.SetAreaClear_Flg = 1;
+        GameDirector.Instance.SetStageClear_Flg = 1;
+
+        // タイトルを非同期で読み込みながらフェード処理
+        StartCoroutine("SceneMoveTitle");
+    }
+
+    // 非同期でタイトルを読み込みながらタイトルへ遷移
+    private IEnumerator SceneMoveTitle()
+    {
+        AsyncOperation title = SceneManager.LoadSceneAsync("Title");                // タイトルを非同期で読み込み
+        title.allowSceneActivation = false;                                         // フェード処理が終わるまではシーン遷移を許可しない(注意点としてallowSceneActivationがfalseのままだとisDoneはfalseのまま)
+
+        FadeImage.gameObject.SetActive(true);                                       // フェード画像のSetActiveをtrueに変更
+
+        // 処理を軽くするためいったん関数を抜ける
+        yield return null;
+
+        while(!title.isDone)        // タイトルの読み込みがまだの時かつ、フェード処理が終わってない時
+        {
+            if(FadeImage.color.a < 1.0f)                // フェード処理がまだの時
+            {
+                // フェード画像のアルファ値を加算
+                FadeImage.color = new Color(FadeImage.color.r, FadeImage.color.g, FadeImage.color.b, FadeImage.color.a + 0.02f);
+            }
+            else                                        // フェード処理が終わったら
+            {
+                title.allowSceneActivation = true;     // シーン遷移を許可
+            }
+
+            yield return null;
+        }
+
+    }
+}
---
+++ Assets/Wakugawa/Scripts/WarmDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarmDisplay : MonoBehaviour {
+
+    [SerializeField] GameObject[] TextButtons;       // 本当にリセットするかどうかの確認ボタンとBGM等のUI
+
+    // 確認ボタンを表示
+    public void WarmTextOnDisplay()
+    {
+        TextButtons[0].gameObject.SetActive(true);  // 確認ボタンのSetActiveをtrueに
+        TextButtons[1].gameObject.SetActive(false); // BGMなどのUIのSetActiveをfalseに
+        gameObject.SetActive(false);                // 自分自身のSetActiveをfalseに
+    }
+
+    // Noボタンを押したとき
+    public void ResetCancel()
+    {
+        TextButtons[0].gameObject.SetActive(true);  // リセットボタンのSetActiveをtrueに
+        TextButtons[1].gameObject.SetActive(true);  // BGMなどのUIのSetActiveをfalseに
+        gameObject.SetActive(false);                // 自分自身の親のSetActiveをfalseに
+    }
+}
